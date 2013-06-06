82cadab277e71a5aeaac712f62b848623b967207
Paul Ozog <pjozog@gmail.com>
2013-06-05 22:50:35 -0400

    Fixed mvnrnd typo

--- PylabUtils/stats/mvnrnd.py
+++ PylabUtils/stats/mvnrnd.py
@@ -8,10 +8,9 @@ def mvnrnd (mu, Sigma, N):
     
     shape = mu.shape
     x = randn (shape[0], N)
-    
     sqrtSigma = cholesky (Sigma)
-    
     y = sqrtSigma.dot(x)
+    ret = y + tile (mu, (N,1)).transpose ()
 
     # this is ugly but I'm not sure how to handle the case if N is 1
     if ret.shape[1] == 1:

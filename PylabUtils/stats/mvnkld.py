678607fbcbf494f91a6b73585dcd033218c638c6
Paul Ozog <pjozog@gmail.com>
2014-04-27 16:16:02 -0400

    KLD: Using solve () instead of inverting directly

--- PylabUtils/stats/mvnkld.py
+++ PylabUtils/stats/mvnkld.py
@@ -17,5 +17,6 @@ def mvnkld (mu0, mu1, sigma0, sigma1):
     (sign0, logdet0) = linalg.slogdet (sigma0)
     (sign1, logdet1) = linalg.slogdet (sigma1)
     lndet = logdet0 - logdet1
-    # lndet = pl.np.log (linalg.det (sigma0) / linalg.det (sigma1))
-    return 0.5 * (trace (linalg.inv (sigma1).dot (sigma0)) + delta.T.dot (sigma1).dot (delta) - k - lndet)
+    A = trace (linalg.solve (sigma1, sigma0))
+    B = delta.T.dot (linalg.solve (sigma1, delta))
+    return 0.5 * (A + B - k - lndet)

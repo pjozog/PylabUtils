1c495c7172119f475d0905356d718adb9b46265a
Paul Ozog <pjozog@gmail.com>
2016-09-29 15:26:07 -0400

    Using UT to compute covariance from acerkmann params, too

--- examples/ackermann.py
+++ examples/ackermann.py
@@ -28,11 +28,11 @@ class Ackermann():
 
 # Shows how to scale time to maintain same distribution with varying timesteps
 if __name__ == '__main__':
-    dt = 0.01                   # time between sensor readings, e.g., changeable
+    dt = 0.1                   # time between sensor readings, e.g., changeable
     a = Ackermann(dt)
     ackermannSigma = eye(3)
 
-    ackermannSigma[0,0] = (1)**2
+    ackermannSigma[0,0] = (10)**2
     ackermannSigma[1,1] = (0.01)**2
     ackermannSigma[2,2] = (0.01)**2
     ackermannSigma /= dt        # need to scale covariance by time!
@@ -44,19 +44,24 @@ if __name__ == '__main__':
     xs = []
     ys = []
 
+    steer_angle = 920 * pi/180.
+    wheel_speed_l = 0.1     # m/s
+    wheel_speed_r = 0.1     # m/s
+    x = np.array([steer_angle, wheel_speed_l, wheel_speed_r])
+    J = plu.diff.numerical_jacobian(a.x_i_iplus1, x)
+
+    sigmaPoints, meanWeight, covWeight = plu.ut.unscented_transform (x, ackermannSigma)
+
+    mu = a.x_i_iplus1(x)
+    Sigma_ = J.dot(ackermannSigma).dot(J.T)
+
+    mu, Sigma_ = plu.ut.unscented_func(a.x_i_iplus1, sigmaPoints, meanWeight,
+                                       covWeight, angleMask=np.array([False, False, True]))
+
     maxsecs = 10                # number of seconds to execute, changeable
     N = int(maxsecs / dt)       # number of discrete steps to compute
     for i in range(N):
-        steer_angle = 920 * pi/180.
 
-        wheel_speed_l = 0.1     # m/s
-        wheel_speed_r = 0.1     # m/s
-        x = np.array([steer_angle, wheel_speed_l, wheel_speed_r])
-
-        J = plu.diff.numerical_jacobian(a.x_i_iplus1, x)
-
-        mu = a.x_i_iplus1(x)
-        Sigma_ = J.dot(ackermannSigma).dot(J.T)
         Sigma = np.zeros((6,6))
         Sigma[0:2,0:2] = Sigma_[0:2,0:2]
         Sigma[5,5] = Sigma_[2,2]
@@ -80,8 +85,8 @@ if __name__ == '__main__':
 
         x_gi_2d = np.array([x_gi[0], x_gi[1], x_gi[2]])
 
-        x, y = plu.plotting.calculateEllipseXY(x_gi[0:2], x_gi_cov[0:2,0:2], 9)
-        plot(x,y)
+        xcov, ycov = plu.plotting.calculateEllipseXY(x_gi[0:2], x_gi_cov[0:2,0:2], 9)
+        plot(xcov, ycov)
         plot(x_gi[0], x_gi[1], 'x')
         axis('equal')
         grid('on')
